d57d719a8397c9ba671fdab8d981bfb0653116d3
Prateek Kanurkar <prateekkanurkar@gmail.com>
2026-07-04 14:50:06 +0530

    Restyle app UI to Google Cloud Console look (Material light theme + top app bar)

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header className="appbar">
+          <div className="appbar-left">
+            <span className="appbar-menu">☰</span>
+            <span className="appbar-brand">ProofOfModel</span>
+            <span className="appbar-project">
+              <b>monad-testnet</b> ▾
+            </span>
+          </div>
+          <div className="appbar-right">
+            <span className="appbar-avatar">S</span>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
